ed49212b184f0279ebce3fe4af1607cb6e8312b5
gvikthor <mohmas@inf.elte.hu>
2023-03-01 15:45:24 +0100

    Gyakorló feladatok 1. gyak

---
+++ 22-23-1/Gyakorló feladatok/01 Alapok/README.md
@@ -0,0 +1,37 @@
+# 1. Gyakorlat - Gyakorló feladatok
+
+## Kutatók
+Egy interdiszciplináris kutatócsoportról szeretnénk statisztikákat készíteni. A kiinduló adatokat a `script.js` file-ban találod.
+
+### Feladatok
+1. Ki a legfiatalabb kutató?
+2. Hány diplomájuk van összesen a kutatóknak?
+3. Találd meg a JavaScript dokumentációban (Mozilla Developer Network) azt a függvényt, amivel új elemet lehet tenni egy tömbbe.
+4. Találd meg a JavaScript dokumentációban azt a függvényt, ami megmondja, hogy egy tömb/string tartalmaz-e egy megadott értéket.
+5. Listázd ki az összes mesterképzés nevét (`title`).
+6. Ki és milyen területen szerezte meg legkorábban a doktori fokozatot? (PhD) Ha senki nem szerzett doktori fokozatot, akkor ezt írd ki.
+7. Mi az átlag életkora azoknak az embereknek, akinek van doktori fokozata?
+
+### Ha jól oldottad meg
+1.
+```
+George Raich
+```
+
+2.
+```
+12
+```
+
+5.
+```
+Historian - Ancient history specialization
+Computer Science - Embedded systems specialization Astrophysics
+Zoologist
+Psychology
+```
+​
+6.
+```
+The earliest PhD degree is Computer Science - Autonomous guided missile systems (2034)
+```
---
+++ 22-23-1/Gyakorló feladatok/01 Alapok/script.js
@@ -0,0 +1,93 @@
+
+[
+    {
+        name: 'Valentine Cherry',
+        age: 28,
+        degrees: [
+            {
+                title: 'Historian',
+                level: 'Bachelor\'s',
+                year: 2030
+            },
+            {
+                title: 'Historian - Ancient history specialization',
+                level: 'Master\'s',
+                year: 2033
+            },
+            {
+                title: 'Historian - European royal families and inheritance',
+                level: 'PhD',
+                year: 2037
+            }
+        ]
+    },
+    {
+        name: 'George Raich',
+        age: 32,
+        degrees: [
+            {
+                title: 'Computer Science',
+                level: 'Bachelor\'s',
+                year: 2027
+            },
+            {
+                title: 'Computer Science - Embedded systems specialization',
+                level: 'Master\'s',
+                year: 2030
+            },
+            {
+                title: 'Computer Science - Autonomous guided missile systems',
+                level: 'PhD',
+                year: 2034
+            }
+        ]
+    },
+    {
+        name: 'Peter Smith',
+        age: 31,
+        degrees: [
+            {
+                title: 'Physics',
+                level: 'Bachelor\'s',
+                year: 2028
+            },
+            {
+                title: 'Astrophysics',
+                level: 'Master\'s',
+                year: 2030
+            }
+        ]
+    },
+    {
+        name: 'Aron Warm',
+        age: 29,
+        degrees: [
+            {
+                title: 'Biologist',
+                level: 'Bachelor\'s',
+                year: 2030
+            },
+            {
+                title: 'Zoologist',
+                level: 'Master\'s',
+                year: 2032
+            }
+        ]
+    },
+    {
+        name: 'Laurie Pakon',
+        age: 32,
+        degrees: [
+            {
+                title: 'Psychology',
+                level: 'Master\'s (undivided)',
+                year: 2030
+            },
+            {
+                title: 'Psychology - Youth mental health',
+                level: 'PhD',
+                year: 2034
+            }
+        ]
+    }
+]
